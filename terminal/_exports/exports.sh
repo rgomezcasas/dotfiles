94f5af0a22d3e48e5c28e1a4b7a3b1421b0c1ca8
Rafa Gómez <rgomezcasas@gmail.com>
2018-09-16 22:23:50 +0200

    Update settings

--- terminal/_exports/exports.sh
+++ terminal/_exports/exports.sh
@@ -5,7 +5,7 @@ export SBT_OPTS='-Xms512M -Xmx1024M -Xss2M -XX:MaxMetaspaceSize=512m -XX:Reserve
 export GOPATH="$HOME/.go"
 export GEM_HOME=$HOME/.gem
 
-export PATH=/usr/local/opt/php@7.1/bin:$PATH:~/bin:/usr/local/bin:/usr/local/sbin:$DOTFILES_PATH/bin:$DOTFILES_PATH/git/bin:~/.composer/vendor/bin:$JAVA_HOME/bin:$GOPATH/bin:$GEM_HOME/bin:/usr/local/opt/python/libexec/bin:/usr/local/opt/fzf/bin:/usr/local/opt/php@7.1/sbin
+export PATH=/usr/local/opt/php@7.1/bin:/usr/local/opt/make/libexec/gnubin:$PATH:~/bin:/usr/local/bin:/usr/local/sbin:$DOTFILES_PATH/bin:$DOTFILES_PATH/git/bin:~/.composer/vendor/bin:$JAVA_HOME/bin:$GOPATH/bin:$GEM_HOME/bin:/usr/local/opt/python/libexec/bin:/usr/local/opt/fzf/bin:/usr/local/opt/php@7.1/sbin
 
 export FZF_DEFAULT_OPTS='--color=bg+:24 --reverse'
 
