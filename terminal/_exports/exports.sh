253908070ac8d880e4206d899ae6f5d67ee0045c
Rafa Gómez <rgomezcasas@gmail.com>
2019-01-03 17:07:43 +0100

    Paths in an easy way to read

--- terminal/_exports/exports.sh
+++ terminal/_exports/exports.sh
@@ -1,11 +1,12 @@
 export MY_SSH_USERNAME='rafa.gomez'
 
 export JAVA_HOME='/Library/Java/JavaVirtualMachines/amazon-corretto-8.jdk/Contents/Home'
-export SBT_OPTS='-Xms512M -Xmx1024M -Xss2M -XX:MaxMetaspaceSize=512m -XX:ReservedCodeCacheSize=256M -Dfile.encoding=UTF8'
+export PHP_PATH='/usr/local/opt/php@7.1'
+export PYTHON_PATH='/usr/local/opt/python'
 export GOPATH="$HOME/.go"
-export GEM_HOME=$HOME/.gem
+export GEM_HOME="$HOME/.gem"
 
-export PATH=/usr/local/opt/php@7.1/bin:/usr/local/opt/make/libexec/gnubin:$PATH:~/bin:/usr/local/bin:/usr/local/sbin:$DOTFILES_PATH/bin:$DOTFILES_PATH/git/bin:~/.composer/vendor/bin:$JAVA_HOME/bin:$GOPATH/bin:$GEM_HOME/bin:/usr/local/opt/python/libexec/bin:/usr/local/opt/fzf/bin:/usr/local/opt/php@7.1/sbin
+export SBT_OPTS='-Xms512M -Xmx1024M -Xss2M -XX:MaxMetaspaceSize=512m -XX:ReservedCodeCacheSize=256M -Dfile.encoding=UTF8'
 
 export FZF_DEFAULT_OPTS='--color=bg+:24 --reverse'
 
@@ -14,3 +15,21 @@ export HOMEBREW_NO_ANALYTICS=true
 export HOMEBREW_INSTALL_BADGE="(ʘ‿ʘ)"
 
 export LANG=en_GB
+
+# @todo Migrate this to an array of paths (I don't like this inline because it's hard to read)
+PATH=/usr/local/sbin:$PATH
+PATH=/usr/local/bin:$PATH
+PATH=/usr/local/opt/fzf/bin:$PATH
+PATH=/usr/local/opt/make/libexec/gnubin:$PATH
+PATH=~/.composer/vendor/bin:$PATH
+PATH=${PHP_PATH}/sbin:$PATH
+PATH=${PHP_PATH}/bin:$PATH
+PATH=${PYTHON_PATH}/libexec/bin:$PATH
+PATH=${GEM_HOME}/bin:$PATH
+PATH=${GOPATH}/bin:$PATH
+PATH=${JAVA_HOME}/bin:$PATH
+PATH=~/bin:$PATH
+PATH=${DOTFILES_PATH}/git/bin:$PATH
+PATH=${DOTFILES_PATH}/bin:$PATH
+
+export PATH=$PATH
