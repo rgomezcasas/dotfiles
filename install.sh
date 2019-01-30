176d266700a6251b17517a62ab3de3406a3d821a
Rafa Gómez <rgomezcasas@gmail.com>
2019-01-30 11:24:40 +0100

    [mac] Add Rocket

--- install.sh
+++ install.sh
@@ -17,6 +17,8 @@ ln -s -i ${DOTFILES_PATH}/mac/karabiner-elements $HOME/.config/karabiner
 # Spectacle
 mkdir -p $HOME/Library/Application\ Support/Spectacle
 ln -s -i ${DOTFILES_PATH}/mac/spectacle/Shortcuts.json $HOME/Library/Application\ Support/Spectacle/Shortcuts.json
+# Rocket
+ln -s ${DOTFILES_PATH}/mac/Rocket $HOME/Library/Application\ Support/Rocket
 # ulimit
 sudo ln -s -i ${DOTFILES_PATH}/mac/plist/limit.maxfiles.plist /Library/LaunchDaemons/limit.maxfiles.plist
 sudo chown root:wheel /Library/LaunchDaemons/limit.maxfiles.plist
