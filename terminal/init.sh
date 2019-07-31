42605ab7cdd517d0befa0d5ec555ea3ab57bb7d0
Rafa Gómez <rgomezcasas@gmail.com>
2019-07-31 23:11:14 +0200

    Improve bash code

--- terminal/init.sh
+++ terminal/init.sh
@@ -1,11 +1,11 @@
 ulimit -n 65536
 ulimit -u 1000
 
-. $HOME/bin/z.sh
+. "$HOME/bin/z.sh"
 
 # Register all aliases
-for aliasToSource in ${DOTFILES_PATH}/terminal/_aliases/*; do source ${aliasToSource}; done
+for aliasToSource in "$DOTFILES_PATH/terminal/_aliases/"*; do source $aliasToSource; done
 # Register all exports
-for exportToSource in ${DOTFILES_PATH}/terminal/_exports/*; do source ${exportToSource}; done
+for exportToSource in "$DOTFILES_PATH/terminal/_exports/"*; do source $exportToSource; done
 # Register all functions
-for functionToSource in ${DOTFILES_PATH}/terminal/_functions/*; do source ${functionToSource}; done
+for functionToSource in "$DOTFILES_PATH/terminal/_functions/"*; do source $functionToSource; done
