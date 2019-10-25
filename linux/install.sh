5ec50065b2fa6e38e2cbafe82b3d4df5fb495166
Rafa Gómez <rgomezcasas@gmail.com>
2019-10-25 16:06:31 +0200

    Remove fonts

--- linux/install.sh
+++ linux/install.sh
@@ -6,7 +6,3 @@ ln -s -i "$DOTFILES_PATH/linux/autostart" "$HOME/.config/"
 
 sudo ln -s -i "$DOTFILES_PATH/linux/configs/keyboard" "/etc/default/keyboard"
 sudo ln -s -i "$DOTFILES_PATH/linux/xorg.conf.d/00-keyboard.conf" "/usr/share/X11/xorg.conf.d/00-keyboard.conf"
-
-mkdir -p "$HOME/.fonts"
-for font in ${DOTFILES_PATH}/fonts/*; do ln -s -i "$font" "$HOME/.fonts/"; done
-,
