b8b5daa87426082531754e5b665eaa70cc2e6efb
Rafa Gómez <rgomezcasas@gmail.com>
2019-11-12 23:17:10 +0100

    Add script to navigate to a chrome tab

--- scripts/core/_main.sh
+++ scripts/core/_main.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-if ! ${DOT_MAIN_SOURCED:-false} ; then
-   source "$DOTFILES_PATH/scripts/core/args.sh"
-   source "$DOTFILES_PATH/scripts/core/collections.sh"
-   source "$DOTFILES_PATH/scripts/core/documentation.sh"
-   source "$DOTFILES_PATH/scripts/core/git.sh"
-   source "$DOTFILES_PATH/scripts/core/log.sh"
-   source "$DOTFILES_PATH/scripts/core/platform.sh"
-   source "$DOTFILES_PATH/scripts/core/table.sh"
+if ! ${DOT_MAIN_SOURCED:-false}; then
+  source "$DOTFILES_PATH/scripts/core/args.sh"
+  source "$DOTFILES_PATH/scripts/core/collections.sh"
+  source "$DOTFILES_PATH/scripts/core/documentation.sh"
+  source "$DOTFILES_PATH/scripts/core/git.sh"
+  source "$DOTFILES_PATH/scripts/core/log.sh"
+  source "$DOTFILES_PATH/scripts/core/platform.sh"
+  source "$DOTFILES_PATH/scripts/core/table.sh"
 
-   readonly DOT_MAIN_SOURCED=true
+  readonly DOT_MAIN_SOURCED=true
 fi
