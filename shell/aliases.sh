df6ac6b5af2dbfead1c2010b73bab13f441edcaf
Rafa Gómez <rgomezcasas@gmail.com>
2020-08-20 14:28:54 +0200

    Add new mouse transformations

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -30,6 +30,7 @@ alias up="dot package update_all"
 alias copy='pbcopy'
 alias dc='dot docker connect'
 alias vcode='/usr/local/bin/code'
+alias vt='vim $(mktemp $TMPDIR/$(uuidgen).txt)'
 
 alias i.='(idea $PWD &>/dev/null &)'
 alias c.='(code $PWD &>/dev/null &)'
