3406a249daea0462e1d9705d2b151f98731fd819
Rafa Gómez <rgomezcasas@gmail.com>
2021-05-29 15:02:26 +0200

    Add light theme for vscode

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,11 +1,26 @@
+# ------------------------------------------------------------------------------
+# Codely theme config
+# ------------------------------------------------------------------------------
+export CODELY_THEME_PROMPT_IN_NEW_LINE=false
+export CODELY_THEME_MODE=false
+
+# ------------------------------------------------------------------------------
+# Languages
+# ------------------------------------------------------------------------------
 export JAVA_HOME='/Library/Java/JavaVirtualMachines/amazon-corretto-16.jdk/Contents/Home'
 export GEM_HOME="$HOME/.gem"
 export GOPATH="$HOME/.go"
 
-export FZF_DEFAULT_OPTS='
-  --color=pointer:#ebdbb2,bg+:#3c3836,fg:#ebdbb2,fg+:#fbf1c7,hl:#8ec07c,info:#928374,header:#fb4934
-  --reverse
-'
+# ------------------------------------------------------------------------------
+# Apps
+# ------------------------------------------------------------------------------
+if [ "$CODELY_THEME_MODE" = "dark" ]; then
+  fzf_colors="pointer:#ebdbb2,bg+:#3c3836,fg:#ebdbb2,fg+:#fbf1c7,hl:#8ec07c,info:#928374,header:#fb4934"
+else
+  fzf_colors="pointer:#db0f35,bg+:#d6d6d6,fg:#808080,fg+:#363636,hl:#8ec07c,info:#928374,header:#fffee3"
+fi
+
+export FZF_DEFAULT_OPTS="--color=$fzf_colors --reverse"
 
 export HOMEBREW_AUTO_UPDATE_SECS=604800 # 1 week
 export HOMEBREW_NO_ANALYTICS=true
@@ -14,9 +29,9 @@ export BAT_THEME='gruvbox-dark'
 
 export GPG_TTY=$(tty)
 
-export LANG="en_US.UTF-8"
-export LC_ALL="en_US.UTF-8"
-
+# ------------------------------------------------------------------------------
+# Path - The higher it is, the more priority it has
+# ------------------------------------------------------------------------------
 export path=(
   "$HOME/bin"
   "$DOTLY_PATH/bin"
--- shell/init.sh
+++ shell/init.sh
@@ -1,6 +1,11 @@
 # This is a useful file to have the same aliases/functions in bash and zsh
-if [[ $__CFBundleIdentifier == "com.jetbrains."* ]] || [[ $__CFBundleIdentifier == "com.microsoft."* ]]; then
-    export CODELY_THEME_MINIMAL=true
+if [[ $__CFBundleIdentifier == "com.jetbrains."* ]]; then
+  export CODELY_THEME_MINIMAL=true
+fi
+
+if [[ $__CFBundleIdentifier == "com.microsoft."* ]]; then
+  export CODELY_THEME_MINIMAL=true
+  export CODELY_THEME_MODE=light
 fi
 
 source "$DOTFILES_PATH/shell/aliases.sh"
