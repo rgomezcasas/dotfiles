4657d03b28902d2ffb78beeed38b05c840d6d155
Rafa Gómez <rgomezcasas@gmail.com>
2024-10-16 00:09:49 +0200

    chore: add system settings to nixos

--- os/mac/nix/flake.nix
+++ os/mac/nix/flake.nix
@@ -112,6 +112,117 @@
 				onActivation.upgrade = true;
 			};
 
+			# https://daiderd.com/nix-darwin/manual/index.html#:~:text=system/version.nix%3E-,system.defaults,-.%22.GlobalPreferences%22.%22com.apple
+			system.defaults = {
+				".GlobalPreferences"."com.apple.mouse.scaling" = 0.875;
+				ActivityMonitor.IconType = 6; # Show CPU history
+				ActivityMonitor.OpenMainWindow = true;
+				ActivityMonitor.ShowCategory = 101; # All Processes, Hierarchally
+				ActivityMonitor.SortColumn = "CPUUsage";
+				ActivityMonitor.SortDirection = 0; # Descending
+				LaunchServices.LSQuarantine = false;
+				NSGlobalDomain.AppleEnableMouseSwipeNavigateWithScrolls = true;
+				NSGlobalDomain.AppleEnableSwipeNavigateWithScrolls = true;
+				NSGlobalDomain.AppleFontSmoothing = null;
+				NSGlobalDomain.AppleICUForce24HourTime = true;
+				NSGlobalDomain.AppleInterfaceStyle = "Dark";
+				NSGlobalDomain.AppleInterfaceStyleSwitchesAutomatically = false;
+				NSGlobalDomain.AppleMeasurementUnits = "Centimeters";
+				NSGlobalDomain.AppleMetricUnits = 1;
+				NSGlobalDomain.ApplePressAndHoldEnabled = true;
+				NSGlobalDomain.AppleShowAllExtensions = true;
+				NSGlobalDomain.AppleShowAllFiles = false;
+				NSGlobalDomain.AppleShowScrollBars = "WhenScrolling";
+				NSGlobalDomain.AppleTemperatureUnit = "Celsius";
+				NSGlobalDomain.InitialKeyRepeat = 12;
+				NSGlobalDomain.KeyRepeat = 1;
+				NSGlobalDomain.NSAutomaticCapitalizationEnabled = false;
+				NSGlobalDomain.NSAutomaticDashSubstitutionEnabled = false;
+				NSGlobalDomain.NSAutomaticInlinePredictionEnabled = true;
+				NSGlobalDomain.NSAutomaticPeriodSubstitutionEnabled = false;
+				NSGlobalDomain.NSAutomaticQuoteSubstitutionEnabled = false;
+				NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false;
+				NSGlobalDomain.NSAutomaticWindowAnimationsEnabled = true;
+				NSGlobalDomain.NSDocumentSaveNewDocumentsToCloud = false;
+				NSGlobalDomain.NSNavPanelExpandedStateForSaveMode = true;
+				NSGlobalDomain.NSNavPanelExpandedStateForSaveMode2 = true;
+				NSGlobalDomain.NSScrollAnimationEnabled = true;
+				NSGlobalDomain.NSTableViewDefaultSizeMode = 3;
+				NSGlobalDomain.NSWindowShouldDragOnGesture = false;
+				NSGlobalDomain._HIHideMenuBar = true;
+				NSGlobalDomain."com.apple.keyboard.fnState" = false;
+				NSGlobalDomain."com.apple.mouse.tapBehavior" = 1;
+				NSGlobalDomain."com.apple.swipescrolldirection" = true;
+
+				SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;
+				WindowManager.EnableStandardClickToShowDesktop = false;
+				WindowManager.GloballyEnabled = false;
+				dock = {
+					enable-spring-load-actions-on-all-items = false;
+					appswitcher-all-displays = true;
+					autohide = true;
+					autohide-delay = 0.20;
+					autohide-time-modifier = 1.0;
+					largesize = 16;
+					launchanim = true;
+					magnification = false;
+					mineffect = "genie";
+					minimize-to-application = true;
+					mru-spaces = false;
+					orientation = "left";
+					persistent-apps = [
+						"/System/Volumes/Data/Applications/Arc.app"
+					];
+					persistent-others = null;
+					show-process-indicators = true;
+					show-recents = false;
+					showhidden = true;
+					slow-motion-allowed = false;
+					tilesize = 43;
+					wvous-bl-corner = 1;
+					wvous-br-corner = 1;
+					wvous-tl-corner = 1;
+					wvous-tr-corner = 1;
+				};
+				finder = {
+					AppleShowAllExtensions = true;
+					AppleShowAllFiles = false;
+					CreateDesktop = true;
+					FXDefaultSearchScope = "SCcf";
+					FXEnableExtensionChangeWarning = false;
+					FXPreferredViewStyle = "clmv";
+					QuitMenuItem = false;
+					ShowPathbar = true;
+					ShowStatusBar = true;
+					_FXShowPosixPathInTitle = false;
+					_FXSortFoldersFirst = true;
+				};
+				loginwindow.DisableConsoleAccess = true;
+				loginwindow.GuestEnabled = false;
+				menuExtraClock.IsAnalog = false;
+				menuExtraClock.Show24Hour = true;
+				menuExtraClock.ShowAMPM = false;
+				menuExtraClock.ShowDate = 1;
+				menuExtraClock.ShowDayOfMonth = true;
+				menuExtraClock.ShowDayOfWeek = true;
+				menuExtraClock.ShowSeconds = false;
+				screencapture.show-thumbnail = false;
+				screensaver.askForPassword = true;
+				spaces.spans-displays = false;
+				trackpad.ActuationStrength = 0;
+				trackpad.Clicking = true;
+				trackpad.Dragging = false;
+				trackpad.TrackpadRightClick = true;
+				trackpad.TrackpadThreeFingerDrag = false;
+				trackpad.TrackpadThreeFingerTapGesture = 0;
+				universalaccess.closeViewScrollWheelToggle = true;
+				universalaccess.closeViewZoomFollowsFocus = true;
+				universalaccess.reduceMotion = false;
+				universalaccess.reduceTransparency = false;
+			};
+			system.keyboard.enableKeyMapping = true;
+			system.keyboard.remapCapsLockToEscape = true;
+
 			# Auto upgrade nix package and the daemon service.
 			services.nix-daemon.enable = true;
 			# nix.package = pkgs.nix;
@@ -120,7 +231,8 @@
 			nix.settings.experimental-features = "nix-command flakes";
 
 			# Create /etc/zshrc that loads the nix-darwin environment.
-			# programs.zsh.enable = true;	# default shell on catalina
+			programs.zsh.enable = true;
+			programs.zsh.enableCompletion = false;
 			# programs.fish.enable = true;
 
 			# Set Git commit hash for darwin-version.
