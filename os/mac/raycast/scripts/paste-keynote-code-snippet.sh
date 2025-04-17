5857598d7544ef6b9353f13518312f3f7393a886
Rafa Gómez <rgomezcasas@gmail.com>
2025-04-17 11:37:14 +0200

    feat: changes

--- os/mac/raycast/scripts/paste-keynote-code-snippet.sh
+++ os/mac/raycast/scripts/paste-keynote-code-snippet.sh
@@ -12,10 +12,8 @@
 # @raycast.author rgomezcasas
 # @raycast.authorURL https://raycast.com/rgomezcasas
 
-# Limpiar archivo temporal si existe
 rm -f /tmp/clipboard.rtf /tmp/clipboard_fixed.rtf
 
-# Guardar el RTF del portapapeles en un archivo temporal
 osascript -e 'set rtfData to the clipboard as «class RTF »' \
     -e 'set tempFile to "/tmp/clipboard.rtf"' \
     -e 'set fileRef to open for access tempFile with write permission' \
@@ -23,16 +21,12 @@ osascript -e 'set rtfData to the clipboard as «class RTF »' \
     -e 'write rtfData to fileRef' \
     -e 'close access fileRef'
 
-# Modificar el tamaño de fuente en el archivo RTF (56 medios puntos ≈ 28px)
-# y eliminar todos los posibles marcadores de fondo
+# Change font size (56 pts ≈ 28px) + remove bg
 perl -i -pe 's/\\fs\d+/\\fs56/g; s/\\highlight\d+\s*//g; s/\\cb\d+\s*//g; s/\\shading\d+\s*//g; s/\\chshdng\d+\s*//g; s/\\chcbpat\d+\s*//g;' /tmp/clipboard.rtf
 
-# Usar textutil para convertir el RTF a RTF (esto puede parecer redundante pero ayuda a normalizar el formato)
 textutil -convert rtf -output /tmp/clipboard_fixed.rtf /tmp/clipboard.rtf
-
-# Copiar el RTF modificado al portapapeles usando pbcopy
 cat /tmp/clipboard_fixed.rtf | pbcopy
 
 osascript -e 'tell application "System Events" to keystroke "v" using command down'
 
-echo "Tamaño de fuente cambiado a 28px y fondo transparente (preservando formato y cursivas)"
+echo "Done!"
