5ff2617aa2905e2ef614fa29da91272df421f90f
Rafa Gómez <rgomezcasas@gmail.com>
2026-03-25 11:35:35 +0100

    feat(shell): add mooc function for parallel docker and dev server startup

--- config/shell/functions.sh
+++ config/shell/functions.sh
@@ -5,7 +5,19 @@ function j() {
   _zlua "$1"
 }
 
-gpg() { 
+gpg() {
   export GPG_TTY=${GPG_TTY:-$(tty)}
   command gpg "$@"
+}
+
+mooc() {
+  cd /Users/rafa.gomez/Code/codely/codely || return
+  osascript -e '
+    tell application "Ghostty"
+      set currentTerm to focused terminal of selected tab of front window
+      set newTerm to split currentTerm direction down
+      input text "cd /Users/rafa.gomez/Code/codely/codely && yarn dev --filter=@codely/mooc\n" to newTerm
+    end tell
+  '
+  docker compose up mooc-postgres
 }
